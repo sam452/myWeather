aa100a4e850861fe766ed01c5873083039989193
sam walton <sam@foofrancine.com>
2013-03-25 16:37:32 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWeather::Application.config.secret_token = 'd141468e505e5d5ec2cd50f0de7b5465e1ca4c4c94b47fd7c0edad87c92e4d771a02e2c32fadd32002abcf966a6e845f37f0e36dc47b7e2e953d5ea160ca4b20'
